d79cacb12610db5d833541c1fa88c35541db3556
Jonathan Rockway <jrockway@cpan.org>
2005-12-07 23:40:46 +0000

    Import of JROCKWAY/Apache-AuthLDAPBind-0.02 from CPAN.
    gitpan-cpan-distribution: Apache-AuthLDAPBind
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JROCKWAY/Apache-AuthLDAPBind-0.02.tar.gz
    gitpan-cpan-author:       JROCKWAY
    gitpan-cpan-maturity:     released

--- lib/Apache/AuthLDAPBind.pm
+++ lib/Apache/AuthLDAPBind.pm
@@ -11,11 +11,11 @@ Apache::AuthLDAPBind - Authentcates a user to Apache by binding to an
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
